b6fa8c54174c12351488360a7592e4b1ba8e1ff7
erickQuit <erick1332@gmail.com>
2024-04-01 16:15:59 -0600

    web server con FastAPI y Unicorn

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,24 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return[1, 2, 3]
+
+@app.get('/contact')
+def get_list():
+    return{'name': 'Platzi'}
+
+@app.get('/main', response_class=HTMLResponse)
+def get_main():    
+    return """      
+        #Regresa un HTML
+        <h1>Hola soy una pagina</h1>
+        <p>soy un parrafo</p>
+    """
 
 def run():
     store.get_categories()
